2ba3e778a7cd63b5c8c58cedb8341542720b8d63
riduanul <riduanul.haque1@gmail.com>
2024-11-05 17:22:52 +0600

    home routes funtionality

--- Routes/homeRoutes.js
+++ Routes/homeRoutes.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const { isAuthenticated } = require("../middlewares/Auth");
-const { getTopSellers } = require("../Controllers/homeController");
+const {
+  getTopSellers,
+  getBestSellingImages,
+  getWeeklyTopSellingImages,
+} = require("../Controllers/homeController");
 
 const router = express.Router();
 
 router.get("/topSeller", isAuthenticated, getTopSellers);
+router.get("/bestSellingImages", isAuthenticated, getBestSellingImages);
+router.get("/weeklyTopImages", isAuthenticated, getWeeklyTopSellingImages);
 
 module.exports = router;
